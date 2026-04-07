3fb8f841c93ccc835eae0ffe777cff0b4aada34d
makelabs04 <makelabsmail@gmail.com>
2026-04-07 16:38:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'facerecog_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    user: process.env.DB_USER || 'u966260443_facerecog',
+    password: process.env.DB_PASSWORD || 'Makelabs@123',
+    database: process.env.DB_NAME || 'u966260443_facerecog',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
